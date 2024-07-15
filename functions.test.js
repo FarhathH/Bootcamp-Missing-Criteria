c8378de45f15cfd56f235249dbe81b05ff7e2227
FarhathH <farhathh0@gmail.com>
2024-07-15 23:12:11 +0100

    Just figuring out how to use testing methods

--- functions.test.js
+++ functions.test.js
@@ -1,69 +1,59 @@
 
 /*
-    first 'try and catch' for 'isEven' method which didn't work. 
-    I made the mistake of checking that the function as whole worked instead of
-    just the specific condition. 
-
-   " //Checking number is even
-    function isEven(number) {
-        if (number % 2 == 0) {
-            return false
-        }
-        return true
-    }"
-
+    Checking that the function checkes the parameter is an evem number
 */
 try 
 {
-
-    function isEven(number) {
-        if (number % 2 == 0) { //According to function, because 4 is seen as false.
+    function isEven(number) { //This function is being tested to check an even or odd number.
+        if (number % 2 == 0) { 
             return false
         }
         return true
     }
-
     console.log(isEven(4)); //Checking function with printed statement.
-    
-} 
-catch (e) 
+    if(isEven(4) == false){
+        throw new Error("4 is an even number"); //Error message to be printed if the condition is met.
+    }    
+}
+catch (error) 
 {
-    console.log("Something is wrong"); // Statement never prints as the function works, but the
+    console.log(error); // Statement never prints as the function works, 
 }
 
-try
-{
- console.log(4 % 2 == 0); //checking the condition, but it returns true.
-}
-catch(e)
-{
-    console.log("Wrong");
-}
 
 /*
-    Now checking the function for school grades
-
-    function setGrade(student) {
-    if (student.score < 40) {
-        student.grade = "F"
-    } else if (40 <= student.score || student.score <= 50) {
-        student.grade = "D"
-    } else if (50 <= student.score || student.score <= 60) {
-        student.grade = "C"
-    } else if (60 <= student.score || student.score <= 70) {
-        student.grade = "B"
-    } else {
-        student.grade = "A"
-    }
-}
+    Now checking the function for school grades using a list
 */
 
 try
 {
+    let student = {
+        name:"Michelle",
+        score:34,
+        grade:""
+    }
+    function setGrade(student) {
+        if (student.score < 40) {
+            student.grade = "F"
+        } else if (40 <= student.score || student.score <= 50) {
+            student.grade = "D"
+        } else if (50 <= student.score || student.score <= 60) {
+            student.grade = "C"
+        } else if (60 <= student.score || student.score <= 70) {
+            student.grade = "B"
+        } else {
+            student.grade = "A"
+        }
+    }
 
-}
-catch(e)
-{
+    console.log(student.grade);
+    if(student.grade != "F"){
+        throw new Error("This isn't right");
     
+    }    
+}
+catch (error)
+{
+    console.log(error.message)
 }
 
